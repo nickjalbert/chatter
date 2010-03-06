11de285c8d8abef504cb7b7a1d06cd82e6ace22f
Nick Jalbert <nickjalbert@gmail.com>
2010-03-06 16:42:33 -0600

    db does sequencing now--still need to work on display logic

--- write.php
+++ write.php
@@ -8,37 +8,82 @@
 
 include 'functions.php';
 
-function getLastSeqnum() {
+function getInsertQuery($time, $name, $words) {
     global $table_name;
-    $last_chat_query = "SELECT seqnum FROM $table_name ";
-    $last_chat_query .= "WHERE time = (SELECT MAX(time) FROM $table_name)";
-    $result = mysql_query($last_chat_query);
-    $arr = mysql_fetch_array($result);
-    $lastseq = $arr["seqnum"];
-    return $lastseq;
+    $insert_query;
+    if ($time == "") {
+        $insert_query = "INSERT INTO $table_name ";
+        $insert_query .= "(name, words) ";
+        $insert_query .= "VALUES (";
+        $insert_query .= "'".$name."',";
+        $insert_query .= "'".$words."')";
+    } else {
+        $insert_query = "INSERT INTO $table_name ";
+        $insert_query .= "(time, name, words) ";
+        $insert_query .= "VALUES (";
+        $insert_query .= "'".$time."',";     
+        $insert_query .= "'".$name."',";
+        $insert_query .= "'".$words."')";
+    }
+    return $insert_query;
+}
+
+function getSelectQuery($size_limit) {
+    global $table_name;
+    $select_query = "SELECT * FROM $table_name ";
+    $select_query .= "ORDER BY time DESC LIMIT 0,$size_limit";
+    return $select_query;
+}
+
+function getTruncateQuery() {
+    global $table_name;
+    $reset_query = "TRUNCATE TABLE $table_name";
+    return $reset_query;
+}
+
+function getDeleteAfterQuery($after) {
+    global $table_name;
+    $delete_query = "DELETE FROM $table_name WHERE time < '$after'";
+    return $delete_query;
+}
+
+function resetSequenceNumber($num, $chat_history_size) {
+    if ($num > 10000) {
+        $result = mysql_query(getSelectQuery($chat_history_size));
+        $i = 0;
+        $save = Array();
+        while ($arr = mysql_fetch_array($result)) {
+            $str = getInsertQuery($arr["time"], $arr["name"], $arr["words"]);
+            $save[$i] = $str;
+            $i++;
+        }
+        mysql_query(getTruncateQuery());
+        $i =0;
+        while ($i < count($save)) {
+            $query = $save[$i];
+            mysql_query($query);
+            $i++;
+        }
+    }
 }
 
 function removeOld($chat_history_size) {
-    global $table_name;
-    $select_query = "SELECT * FROM $table_name ";
-    $select_query .= "ORDER BY time DESC LIMIT 0,$chat_history_size";
-    $result = mysql_query($select_query);
+    $result = mysql_query(getSelectQuery($chat_history_size));
     $last_date;
+    $last_seq;
     while ($arr = mysql_fetch_array($result)) {
         $last_date = $arr["time"];
+        $last_seq = $arr["seqnum"];
     }
-
-    $delete_query = "DELETE FROM $table_name WHERE time < '$last_date'";
-    mysql_query($delete_query);
+    mysql_query(getDeleteAfterQuery($last_date));
+    resetSequenceNumber($last_seq, $chat_history_size);
 }
 
 $conn = connectToDB();
-$next_seqnum = getLastSeqnum() + 1;
 if ($next_seqnum > $seqnum_max) {
     $next_seqnum = 0;
 }
 
-//removeNextSeqNum($next_seqnum);
 $name = $_REQUEST["name"];
 $words = $_REQUEST["words"];
 
@@ -52,13 +97,7 @@ if ($words == "") {
 if ($name == "") {
     $name = "ScaryGhost";
 }
-
-$insert_query = "INSERT INTO $table_name ";
-$insert_query .= "(time, seqnum, name, words) ";
-$insert_query .= "VALUES (NOW(),'".$next_seqnum."',";
-$insert_query .= "'".$name."',";
-$insert_query .= "'".$words."')";
-
+$insert_query = getInsertQuery("", $name, $words);
 mysql_query($insert_query);
 
 removeOld($chat_history_size);
